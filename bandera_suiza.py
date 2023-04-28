76a96aefd3dd5eeab26e54d06ed40b2a1f1712ab
lizethcifuentes <licethcifuentes@colegioguanenta.edu.co>
2023-04-28 07:29:51 -0500

    se agregan los cambios

---
+++ bandera_suiza.py
@@ -0,0 +1,39 @@
+# se importa la libreria tkinter con todoas sus fnciones
+from tkinter import *
+
+#-----------------------
+# funciones de la app
+#-----------------------
+
+#-----------------------------
+# ventana principal de la app
+#-----------------------------
+
+# se declara una variable llamada ventana_pricipal,que aquiere las carateristicas de un objeto TK[]
+ventana_principal = Tk () 
+
+# titulo de la ventana
+ventana_principal.title ("bandera de francia ")
+# ventana tamaño
+ventana_principal.geometry("500x500")
+# desabilitar boton de maximizar
+ventana_principal.resizable(False,False)
+# color de fondo de la ventana
+ventana_principal.config(bg="red")
+#---------------
+# frama azul
+#---------------
+frame_azul = Frame(ventana_principal)
+frame_azul.config(bg="white", width=300, height=100)
+frame_azul.place(x=100, y=200)
+
+#---------------
+# frama blanca
+#---------------
+frame_azul = Frame(ventana_principal)
+frame_azul.config(bg="white", width=100, height=300)
+frame_azul.place(x=200, y=100 )
+
+# run
+# se ejecuta el emtodo mainloop()de la clase Tk() a travez de la ventana_principal. este metodo despliega la ventana en pantalla y queda ala espera de lo que el usuario haga (click en un boton,escribir,etc). cada accion del usuario se conoce como un evento. El metodo mainloop() es un bucle infventana_principal.mainloop()es un bucle infinito.
+ventana_principal.mainloop()
